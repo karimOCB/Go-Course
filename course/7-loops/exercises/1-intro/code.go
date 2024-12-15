abfe19fbbf73e333f6d3815264af72a493531f36
Karim <chaya.karim.omar@gmail.com>
2024-12-15 12:38:48 -0300

    0.05 Loops 15/12/2024

--- course/7-loops/exercises/1-intro/code.go
+++ course/7-loops/exercises/1-intro/code.go
@@ -5,7 +5,11 @@ import (
 )
 
 func bulkSend(numMessages int) float64 {
-	// ?
+	cost := 0.00
+	for i := 0; i < numMessages; i++ {
+		cost += 1 + (float64(i) * 0.01)
+	}
+	return cost
 }
 
 // don't edit below this line
--- course/7-loops/exercises/5-continue_and_break/code.go
+++ course/7-loops/exercises/5-continue_and_break/code.go
@@ -5,7 +5,25 @@ import (
 )
 
 func printPrimes(max int) {
-	// ?
+	for n := 2; n <= max; n++ {
+		if n == 2 {
+			fmt.Println(n)
+		} 
+		if n % 2 == 0 {
+			continue
+		} 
+		isPrime := true
+		for i := 3; i * i < n + 1 ; i++ {
+			if (n % i == 0) {
+				isPrime = false
+				break
+			}
+		}
+		if !isPrime {
+			continue
+		}
+		fmt.Println(n)
+	}
 }
 
 // don't edit below this line
